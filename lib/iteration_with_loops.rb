7f78c2507b872f3a144e273b45d806b8c0fe419c
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-17 23:04:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,9 @@ def join_nested_strings(src)
   
   new_array = []
   row_index = 0 
-  while row_index < src.length do
+  while row_index < src.count do
     element_index = 0 
-    while element_index < src[row_index].length do
+    while element_index < src[row_index].count do
       if src[row_index][element_index].class == String 
         new_array << src[row_index][element_index]
       end
